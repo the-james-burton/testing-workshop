24b58c6c3b40ed8b11278c5bf2e31ccbee7d21d4
the-james-burton <the.james.burton@gmail.com>
2018-09-25 07:05:04 +0100

    parse content of file

--- src/main/java/org/projects/workshop/testing/ProcessFile.java
+++ src/main/java/org/projects/workshop/testing/ProcessFile.java
@@ -1,6 +1,9 @@
 package org.projects.workshop.testing;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProcessFile {
@@ -8,13 +11,25 @@ public class ProcessFile {
   public static void main(final String... args) throws Exception {
     File file = new File("/home/jimsey/Development/projects/testing-workshop/src/main/resources/lse-companies.tsv");
     Scanner input = new Scanner(file);
-    input.useDelimiter("\t");
+    input.useDelimiter("\n");
 
+    List<String> headers = new ArrayList<>();
+    List<List<String>> rows = new ArrayList<>();
+
+    if (input.hasNext()) {
+      String[] firstLine = input.next().split("\t");
+      headers.addAll(Arrays.asList(firstLine));
+    } else {
+      System.out.println("no first line!");
+    }
+    
     while(input.hasNext()) {
-        String nextLine = input.nextLine();
-        System.out.println(nextLine);
+        String[] line = input.next().split("\t");
+        rows.add(Arrays.asList(line));
     }
 
+    System.out.println(headers);
+    System.out.println(rows);
     input.close();
   }
   
