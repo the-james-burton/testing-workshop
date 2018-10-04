57d0e725b9eb862cbb2d8542a88167553ca4668c
the-james-burton <the.james.burton@gmail.com>
2018-10-04 07:43:53 +0100

    remove runnable interface

--- src/main/java/org/projects/workshop/testing/ApplicationMain.java
+++ src/main/java/org/projects/workshop/testing/ApplicationMain.java
@@ -5,7 +5,8 @@ public class ApplicationMain {
   public static void main(final String... args) {
     ProcessFile processFile = new ProcessFile();
     processFile.setFilename("src/main/resources/lse-companies.tsv");
-    processFile.run();
+    String result = processFile.process();
+    System.out.println(result);
   }
 
 }
