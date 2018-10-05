e38f4b8e2c974415f59796004385490438efd310
the-james-burton <the.james.burton@gmail.com>
2018-10-05 07:15:10 +0100

    added chapter12 solution

--- src/test/java/org/projects/workshop/testing/ProcessFileTest.java
+++ src/test/java/org/projects/workshop/testing/ProcessFileTest.java
@@ -1,73 +1,34 @@
 package org.projects.workshop.testing;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.projects.workshop.testing.model.ParsedFile;
 import org.projects.workshop.testing.reports.Report;
 
 public class ProcessFileTest {
 
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
-
   private ProcessFile processFile;
-
-  private ParsedFile parsedFile;
-
-  @Mock
-  FileParser mockFileParser;
-
-  @Mock
-  Report mockLongReport;
-
-  @Mock
-  Report mockMarketCapSummaryReport;
 
   @Before
   public void setUp() {
-    MockitoAnnotations.initMocks(this);
-    parsedFile = new ParsedFile();
+    Report mockLongReport = Mockito.mock(Report.class);
+    Report mockMarketCapSummaryReport = Mockito.mock(Report.class);
 
-    when(mockFileParser.parseFile(anyString()))
-        .thenReturn(parsedFile);
-    when(mockLongReport.generateReport(any(ParsedFile.class)))
+    Mockito.when(mockLongReport.generateReport(Mockito.any(ParsedFile.class)))
         .thenReturn("longReportOutput");
-    when(mockMarketCapSummaryReport.generateReport(any(ParsedFile.class)))
+    Mockito.when(mockMarketCapSummaryReport.generateReport(Mockito.any(ParsedFile.class)))
         .thenReturn("marketCapSummaryReportOutput");
 
     processFile = new ProcessFile();
-    processFile.setFileParser(mockFileParser);
+    processFile.setFileParser(new FileParser());
     processFile.setReportGenerator1(mockLongReport);
     processFile.setReportGenerator2(mockMarketCapSummaryReport);
-
-  }
-
-  @Test
-  public void runShouldThrowExceptionIfEmptyFile() {
-    thrown.expect(RuntimeException.class);
-    thrown.expectMessage("no first line!");
-    processFile.setFilename("src/test/resources/lse-companies-empty-file.tsv");
-    processFile.process();
-  }
-
-  @Test(expected = RuntimeException.class)
-  public void runShouldThrowExceptionIfNoFile() {
-    processFile.setFilename("no-file");
-    processFile.process();
   }
 
   @Test
   public void runShouldRunWithoutError() {
     processFile.setFilename("src/test/resources/lse-companies-with-no-mkt-cap.tsv");
     processFile.process();
-    verify(mockLongReport).generateReport(parsedFile);
-
   }
 }
